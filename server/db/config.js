150e3d5a120451f4a29f8fe5372fdf7b1c0b51c4
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2020-03-26 20:50:00 -0300

    Add project boilerplate

--- server/db/config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// DB Setup
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-});
